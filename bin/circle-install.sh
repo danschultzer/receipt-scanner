73ed88f7a10c87d7ed97b01821a0e0771d75e768
Dan Schultzer <dan@dreamconception.com>
2016-07-30 16:47:57 -0500

    Update circle ci install handling

--- bin/circle-install.sh
+++ bin/circle-install.sh
@@ -3,11 +3,15 @@ cd ~
 sudo apt-get install build-essential
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
-wget https://github.com/Itseez/opencv/archive/3.1.0.zip -O opencv-3.1.0.zip
-unzip opencv-3.1.0.zip
+if [ ! -d "opencv-3.1.0" ]; then
+  wget https://github.com/Itseez/opencv/archive/3.1.0.zip -O opencv-3.1.0.zip
+  unzip opencv-3.1.0.zip
+fi
 cd opencv-3.1.0
 sudo cp 3rdparty/ippicv/unpack/ippicv_lnx/lib/intel64/libippicv.a /usr/local/lib/
-mkdir build
+if [ ! -d "build" ]; then
+  mkdir build
+fi
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j $(nproc)
