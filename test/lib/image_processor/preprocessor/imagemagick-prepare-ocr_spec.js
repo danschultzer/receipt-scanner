5a040eb26ead3f3812ec1508c946deed25377fda
Benjamin Schultzer <benjamin@schultzer.com>
2016-10-05 14:04:00 -0700

    Added specs for image processors and refactored
    added specs for
    opencv-phto2scan.js
    graphicsmagick.js
    imagemagick-prepare-ocr.js
    
    added specs for  chained image processors
    in processor_spec.js
    
    added two new test images

---
+++ test/lib/image_processor/preprocessor/imagemagick-prepare-ocr_spec.js
@@ -0,0 +1,19 @@
+var chai = require('chai'),
+  assert = chai.assert;
+
+var scanner = require('../../../../lib/processor');
+
+describe("#imagemagick()", function(done) {
+  this.timeout(30000);
+
+  it("should parse with imagemagick-prepare-ocr as preprocessor", function(done) {
+    scanner(__dirname + "/../../../test_files/readable.jpg")
+      .imagePreprocessor('imagemagick-prepare-ocr')
+      .parse(function(error, results) {
+        assert.equal(error, null);
+        assert.equal(results.amount, "5,280.00");
+        assert.equal(results.date, "2015-08-04");
+        done();
+      });
+  });
+});
---
+++ test/lib/image_processor/preprocessor/opencv-photo2scan_spec.js
@@ -0,0 +1,34 @@
+var chai = require('chai'),
+  assert = chai.assert;
+
+var scanner = require('../../../../lib/processor');
+
+describe("#opencv()", function(done) {
+  this.timeout(30000);
+  
+  it("should parse with onlyFindTextInsidePaperContour", function(done) {
+    scanner(__dirname + "/../../../test_files/readableRotatedGrayscale.png")
+      .imagePreprocessor(['opencv-photo2scan', {
+        onlyFindTextInsidePaperContour: true,
+      }])
+      .parse(function(error, results) {
+        assert.equal(error, null);
+        assert.equal(results.amount, "5,280.00");
+        assert.equal(results.date, "2015-08-04");
+        done();
+      });
+  });
+
+  it("should parse with removeNoise", function(done) {
+    scanner(__dirname + "/../../../test_files/readableRotatedColor.png")
+      .imagePreprocessor(['opencv-photo2scan', {
+        removeNoise: true,
+      }])
+      .parse(function(error, results) {
+        assert.equal(error, null);
+        assert.equal(results.amount, "5,280.00");
+        assert.equal(results.date, "2015-08-04");
+        done();
+      });
+  });
+});
